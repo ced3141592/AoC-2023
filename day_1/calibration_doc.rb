c4fc30e24b62769e5ea30bb8aba17ffc29648e66
Cedric Stürmer <cedricsturmer@MacBook-Pro-von-Cedric.local>
2023-12-02 23:39:59 +0100

    solution part 2

--- day_1/calibration_doc.rb
+++ day_1/calibration_doc.rb
@@ -1,22 +1,41 @@
-def find_first_digit(line)
+INPUT = "input.txt"
+
+def find_composed_number(line)
+  first = nil
+  last = nil
+  i = 0
   line.each_char do |char|
-    return char if char.to_i > 0
+    num = nil
+    num = char.to_i if char.to_i > 0
+    num ||= get_verbose_number(line[i..-1])
+    first ||= num
+    last = num unless num.nil?
+    i += 1
   end
+  (first.to_s + last.to_s).to_i
 end
 
-def merge_numbers(s)
-  last_number = find_first_digit(s.reverse)
-  first_number = find_first_digit(s)
-  (first_number + last_number).to_i unless last_number.nil?
+def get_verbose_number(s)
+  return nil if s.length < 3
+
+  numbers = {
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9,
+  }
+
+  numbers.each_key { |key| return numbers[key] if s.start_with?(key) }
+  nil
 end
 
 def main
-  i = 0
-  File.readlines("input.txt").each do |line|
-    i += merge_numbers(line)
-  end
-
-  puts i
+  puts File.readlines(INPUT).sum { |line| find_composed_number(line) }
 end
 
 main
